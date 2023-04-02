4867985660da2bce5167d8b2b4007162c1e996c3
b3nk3lly <benjkelly98@gmail.com>
2023-04-02 20:54:56 -0300

    Moved logic for fetching and filtering Pokemon to App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,31 @@
 import { Grid } from "@mui/material";
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "../App.css";
 import Filter from "../objects/Filter";
 import filterReducer from "../reducers/FilterReducer";
 import PokemonTable from "./PokemonTable";
 import Settings from "./Settings";
+import pokemonJSON from "../data/pokemon.json";
+import Pokemon from "../objects/Pokemon";
 
 function App() {
+	let [pokemonList, setPokemonList] = useState([] as Pokemon[]);
 	let [filter, filterDispatch] = useReducer(filterReducer, new Filter());
+
+	// fetch Pokemon from JSON when page loads
+	useEffect(() => {
+		setPokemonList(
+			pokemonJSON.map((pokemon) => {
+				return Pokemon.fromJSON(pokemon);
+			})
+		);
+	}, [setPokemonList]);
 
 	return (
 		<div className="App">
 			<Grid container>
 				<Grid item xs={9}>
-					<PokemonTable filter={filter} />
+					<PokemonTable pokemonList={filter.apply(pokemonList)} />
 				</Grid>
 				<Grid item xs={3}>
 					<Settings dispatch={filterDispatch} />
--- src/components/PokemonTable.tsx
+++ src/components/PokemonTable.tsx
@@ -1,24 +1,18 @@
 import Pokemon from "../objects/Pokemon";
-import pokemonJSON from "../data/pokemon.json";
 import { Table } from "@mui/material";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-import Filter from "../objects/Filter";
 import ProgressButton from "./ProgressButton";
 import ProgressType from "../enums/ProgressType";
 
-function getPokemon(filter: Filter): Pokemon[] {
-	let pokemonList = pokemonJSON.map((pokemon) => {
-		return Pokemon.fromJSON(pokemon);
-	});
-
-	return filter.apply(pokemonList);
+interface Props {
+	pokemonList: Pokemon[];
 }
 
-export default function PokemonTable({ filter }: { filter: Filter }) {
+export default function PokemonTable(props: Props) {
 	return (
 		<TableContainer>
 			<Table>
@@ -35,7 +29,7 @@ export default function PokemonTable({ filter }: { filter: Filter }) {
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{getPokemon(filter).map((pokemon: Pokemon) => (
+					{props.pokemonList.map((pokemon: Pokemon) => (
 						<TableRow key={pokemon.id}>
 							<TableCell>{pokemon.number}</TableCell>
 							<TableCell>{pokemon.name}</TableCell>
